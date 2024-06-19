bb54f92849696e057dd4e63d4179272c5ce74f5b
matias <stanghelinimatias@gmail.com>
2024-06-19 02:08:52 -0300

    Deploy the loader in the component

--- src/views/PokemonFavoriteView.vue
+++ src/views/PokemonFavoriteView.vue
@@ -1,25 +1,38 @@
 <template>
-  <v-container class="text-center">
-    <v-row no-gutters>
-      <v-col cols="12" md="8" class="mx-auto">
-        <PokemonListFavorite />
-      </v-col>
-    </v-row>
-  </v-container>
-  <v-container class="text-center">
-    <PokemonListFooter />
+  <v-container class="text-center mx-auto">
+    <PokemonLoader v-if="showLoader" />
+    <template v-else>
+      <v-row no-gutters>
+        <v-col cols="12" md="8" class="mx-auto">
+          <PokemonListFavorite />
+        </v-col>
+      </v-row>
+      <PokemonListFooter />
+    </template>
   </v-container>
 </template>
 
 <script>
 import PokemonListFooter from "../components/footer/PokemonListFooter.vue";
 import PokemonListFavorite from "../components/PokemonListFavorite.vue";
+import PokemonLoader from "../components/loaders/PokemonLoader.vue";
 
 export default {
   name: "PokemonListFavoriteView",
   components: {
     PokemonListFooter,
     PokemonListFavorite,
+    PokemonLoader,
+  },
+  data() {
+    return {
+      showLoader: true,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showLoader = false;
+    }, 2000);
   },
 };
 </script>
