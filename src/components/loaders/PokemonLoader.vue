d74bfed701f8a41cb2acd89f41c31cc06e8704f0
matias <stanghelinimatias@gmail.com>
2024-06-18 21:51:26 -0300

    I use a pokedex image to build a loader.

--- src/components/loaders/PokemonLoader.vue
+++ src/components/loaders/PokemonLoader.vue
@@ -1,7 +1,33 @@
-// crear un loader de pokemon
-
 <template>
-  <div class="loader"></div>
-  <div class="loader"></div>
-  <div class="loader"></div>
+  <div class="loader">
+    <img src="@/assets/images/pokedex.png" height="100px" />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Loader",
+};
+</script>
+
+<style scoped>
+.loader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+}
+
+.loader img {
+  animation: spin 1s infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
