0a80580822780582b5e48556397508d34a67d605
matias <stanghelinimatias@gmail.com>
2024-06-19 02:07:55 -0300

    I create two new views, one to welcome the user and one to return to home.

---
+++ src/views/GoBackHomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row class="text-center">
+    <v-col cols="12">
+      <h1>Uh-oh!</h1>
+    </v-col>
+    <v-col cols="12">
+      <p>You look lost on your journey!</p>
+    </v-col>
+    <v-col cols="12">
+      <GenericShortButton @click="$router.push('/')" text="Go Back Home" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import GenericShortButton from "../components/buttons/GenericShortButton.vue";
+export default {
+  name: "GoBackHomeView",
+  components: {
+    GenericShortButton,
+  },
+};
+</script>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container
+    class="fill-height d-flex flex-column justify-center text-center"
+  >
+    <v-row class="mb-10">
+      <v-col cols="12">
+        <h1>¡Hi there!</h1>
+      </v-col>
+      <v-col cols="12">
+        <p>Welcome to your Pokemon Journey</p>
+      </v-col>
+      <v-col cols="12">
+        <p>Start typing to search for your favorite pokemons</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "WelcomeView",
+};
+</script>
