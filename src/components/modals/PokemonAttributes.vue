90fd35de4692cacd7a5356c343e98278a5ec2a72
matias <stanghelinimatias@gmail.com>
2024-06-21 18:09:03 -0300

    I took responsibility for the modal detail with the creation of the particular attributes of each pokemon.

---
+++ src/components/modals/PokemonAttributes.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-img height="150" src="@/assets/images/background-pokemon.png" cover>
+    <v-container>
+      <v-img height="100" :src="image" />
+    </v-container>
+  </v-img>
+  <v-card-item><strong>Name:</strong> {{ name }} </v-card-item>
+  <v-divider />
+  <v-card-item><strong>Weight:</strong> {{ weight }} </v-card-item>
+  <v-divider />
+  <v-card-item><strong>Height:</strong> {{ height }} </v-card-item>
+  <v-divider />
+  <v-card-item><strong>Type:</strong> {{ types }} </v-card-item>
+  <v-divider />
+</template>
+
+<script>
+export default {
+  name: "PokemonAttributes",
+  props: ["name", "image", "weight", "height", "types"],
+};
+</script>
